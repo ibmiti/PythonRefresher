02e86a142f11d48970a535b688271fa44a73e444
Bmiti <ibmiti@gmail.com>
2020-06-01 16:17:21 -0400

    finishing loops.py

---
+++ loops.py
@@ -0,0 +1,12 @@
+dog_names = ["Fido","Mido","Cleo","Archie"]
+
+for dog in dog_names :
+    print(dog)
+    
+age = 0
+while  age < 18 :
+    print(age)
+    age += 1
+    
+for x in range(1,10):
+    print(x)    
